230f0e9c79f26ef9e910e993d0c63a6b628ac981
sashank27 <sashankmishra27@gmail.com>
2018-03-03 22:19:40 +0530

    Bottom NavBar added

---
+++ app/src/main/java/org/aparoksha/app18/HomeFragment.kt
@@ -0,0 +1,18 @@
+package org.aparoksha.app18
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by sashank on 3/3/18.
+ */
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
--- app/src/main/java/org/aparoksha/app18/MainActivity.kt
+++ app/src/main/java/org/aparoksha/app18/MainActivity.kt
@@ -2,11 +2,43 @@ package org.aparoksha.app18
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_schedule -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+                .add(R.id.containerFrame, HomeFragment())
+                .commit()
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.containerFrame,  fragment)
+                .commit()
     }
 }
