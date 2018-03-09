bdb1bbb4e0f970506aa2cf4b9ef6f552257bcd78
sashank27 <sashankmishra27@gmail.com>
2018-03-10 03:50:25 +0530

    EventDetail almost finished

--- app/src/main/java/org/aparoksha/app18/activities/EventDetailActivity.kt
+++ app/src/main/java/org/aparoksha/app18/activities/EventDetailActivity.kt
@@ -7,11 +7,11 @@ import jp.wasabeef.blurry.Blurry
 import kotlinx.android.synthetic.main.activity_event_detail.*
 import org.aparoksha.app18.R
 import android.graphics.BitmapFactory
-import android.graphics.Bitmap
-import com.richpath.RichPath
-import com.richpath.RichPathView
-import com.richpathanimator.RichPathAnimator
-
+import org.aparoksha.app18.adapters.EventDetailViewPagerAdapter
+import org.aparoksha.app18.models.Event
+import org.aparoksha.app18.ui.GlideApp
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EventDetailActivity : AppCompatActivity() {
 
@@ -21,9 +21,117 @@ class EventDetailActivity : AppCompatActivity() {
 
         val image = BitmapFactory.decodeResource(resources, R.drawable.hint)
         Blurry.with(this@EventDetailActivity)
-                .color(Color.argb(120, 0, 0, 0))
+                .radius(40)
+                .sampling(2)
+                .color(Color.argb(120,0,0,0))
                 .async()
+                .animate(500)
                 .from(image)
                 .into(eventFirstIV)
+
+        val eventID = intent.getLongExtra("id",0)
+        if (eventID != 0L) setEventDetails(eventID)
+        setViewPager(eventID)
     }
+
+    private fun setViewPager(eventID: Long) {
+        navigationStrip.setTitles("Description", "Organizers", "Updates")
+        viewPager.adapter = EventDetailViewPagerAdapter(supportFragmentManager,eventID)
+        navigationStrip.setViewPager(viewPager)
+    }
+
+    private fun setEventDetails(eventID: Long) {
+        val event = Event()
+
+        eventTitleTV.text = event.name
+        //eventDescriptionTV.text = event.description
+        //eventFbLinkTV.text = event.facebookEventLink
+
+        val calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/India"))
+        if (event.timestamp < 100L) {
+            eventTimeTV.text = "Online Event"
+            //reminderTV.text = "Online Event"
+            //reminderRL.isClickable = false
+            eventLocationTV.text = "Online"
+        } else {
+            calendar.timeInMillis = event.timestamp.times(1000L)
+
+            val sdf = SimpleDateFormat("MMMM d, hh:mm a")
+            sdf.timeZone = TimeZone.getTimeZone("Asia/India")
+
+            eventTimeTV.text = sdf.format(calendar.time)
+            eventLocationTV.text = event.location
+        }
+
+        if (event.imageUrl != "") {
+            GlideApp.with(this).load(event.imageUrl).into(eventSecondIV)
+
+            Blurry.with(this@EventDetailActivity)
+                    .color(Color.argb(120, 0, 0, 0))
+                    //.onto(eventFirstIV)
+        }
+
+        /*event.organizers.map {
+            val view = View.inflate(this, R.layout.organizer_layout, null)
+            view.organizerNameTV.text = it.name
+            callNumber = it.phoneNumber.toString()
+            view.positionTextView.text = callNumber
+
+            view.button2.setOnClickListener({
+                if (ContextCompat.checkSelfPermission(this,
+                                Manifest.permission.CALL_PHONE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            arrayOf(Manifest.permission.CALL_PHONE),
+                            requestCode)
+                }
+                else
+                    startActivity(Intent(Intent.ACTION_CALL).setData(Uri.parse("tel:" + callNumber)))
+            })
+
+            organizerLinearLayout.addView(view)
+        }
+*/
+        /*if (event.facebookEventLink.isBlank()) {
+            facebookLinkLayout.visibility = View.GONE
+        }
+
+        if (event.organizers.isEmpty()) {
+            organizersLayout.visibility = View.GONE
+        }
+
+        if (event.additionalInfo.isEmpty()) {
+            additionalInfoLayout.visibility = View.GONE
+        } else {
+            additionalInfoTextView.text = event.additionalInfo.joinToString { "\n" }
+        }
+
+        */
+
+        /*reminderRL.setOnClickListener({
+            if ((event.timestamp - 330 * 60) > System.currentTimeMillis() / 1000L) {
+                toast("Reminder Added Successfully!!")
+                if (event.location.isEmpty())
+                    remindForEvent(event.timestamp, "Reminder!!",
+                            event.name + " is about to start!")
+                else
+                    remindForEvent(event.timestamp, "Reminder!!",
+                            event.name + " is about to start. Reach " + event.location + "!")
+            }
+        })
+
+        facebookLinkLayout.setOnClickListener({
+            val url = event.facebookEventLink
+            try {
+                baseContext.packageManager.getPackageInfo("com.facebook.katana", 0)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("fb://facewebmodal/f?href=" + url))
+                baseContext.startActivity(intent)
+            } catch (e: Exception) {
+                val builder = CustomTabsIntent.Builder()
+                val customTabsIntent = builder.build()
+                customTabsIntent.launchUrl(this, Uri.parse(url))
+            }
+        })*/
+    }
+
 }
---
+++ app/src/main/java/org/aparoksha/app18/fragments/EventDescriptionFragment.kt
@@ -0,0 +1,66 @@
+package org.aparoksha.app18.fragments
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_description.*
+import org.aparoksha.app18.R
+import org.aparoksha.app18.models.Event
+
+/**
+ * Created by sashank on 10/3/18.
+ */
+
+class EventDescriptionFragment : Fragment() {
+
+    fun newInstance(eventID: Long): EventDescriptionFragment {
+
+        val args = Bundle()
+        args.putLong("eventId", eventID)
+        val fragment = EventDescriptionFragment()
+        fragment.arguments = args
+        return fragment
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_description, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val eventID = arguments.getLong("eventId")
+
+        val event = Event()
+
+        /*descriptionTV.text = event.description
+
+        if (event.facebookEventLink.isBlank()) {
+            facebookLinkLayout.visibility = View.GONE
+        }
+
+        if (event.additionalInfo.isEmpty()) {
+            additionalInfoLayout.visibility = View.GONE
+        } else {
+            additionalInfoTV.text = event.additionalInfo.joinToString { "\n" }
+        }*/
+
+        facebookLinkLayout.setOnClickListener({
+            val url = event.facebookEventLink
+            try {
+                activity.packageManager.getPackageInfo("com.facebook.katana", 0)
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("fb://facewebmodal/f?href=$url"))
+                activity.startActivity(intent)
+            } catch (e: Exception) {
+                val builder = CustomTabsIntent.Builder()
+                val customTabsIntent = builder.build()
+                customTabsIntent.launchUrl(activity, Uri.parse(url))
+            }
+        })
+    }
+}
